bedd1c207582925344264545b72123eea57208e8
IDepp3 <alvarez.joaquinez@gmail.com>
2021-11-22 02:17:58 -0300

    t3

--- config.sh
+++ config.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# Cambio de propietario de la ruta /opt/
 sudo chown depp3:depp3 /opt/
+
+# Carpeta de configuracion 
+mkdir /opt/config/
+
+# archivos de configuracion vim 
+git clone https://github.com/IDepp3/vim.git /opt/config/vim
+
+# instalando vim
+sudo apt install vim
+

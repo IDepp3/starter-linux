bed2ade0f5d7d705a3e03ee8f5591baf2f63fa7d
IDepp3 <alvarez.joaquinez@gmail.com>
2021-11-22 16:44:21 -0300

    mas configuracion de vscode

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ./colors.sh
 
 # actualizacion del sistema
 sudo apt update
@@ -7,11 +8,8 @@ sudo apt upgrade -y
 # Cambio de propietario de la ruta /opt/
 sudo chown depp3:depp3 /opt/
 
-# Carpeta de configuracion 
-mkdir /opt/config/
-
 # archivos de configuracion vim 
-git clone https://github.com/IDepp3/vim.git /opt/config/vim
+git clone https://github.com/IDepp3/config-linux.git /opt/config
 
 # instalando vim
 sudo apt install vim -y
@@ -22,6 +20,10 @@ rm ~/.vimrc
 # creamos simbolico con la configuracion 
 ln -s /opt/config/vim/.vim ~/.vim
 ln -s /opt/config/vim/.vimrc ~/.vimrc
+
+echo -e "$GREEN [FINALIZO LA INSTALACION Y CONFIGURACION DE VIM]"
+echo -e "PRESIONE UNA TECLA PARA CONTINUAR... $END_COLOR"
+read
 
 # instalando snap
 NOSNAP=/etc/apt/preferences.d/nosnap.pref
@@ -35,3 +37,22 @@ snap install code --classic
 snap install intellij-idea-community --classic
 snap install notion-snap
 snap install slack --classic
+
+echo -e "$GREEN [FINALIZO LA INSTALACION DE SNAP, INTELLIJ, NOTION, SLACK]"
+echo -e "PRESIONE UNA TECLA PARA CONTINUAR... $END_COLOR"
+read
+
+# creamos simbolicos con la configuracion de vscode
+ln -s /opt/config/.vscode/settings.json ~/.config/Code/User/
+ln -s /opt/config/.vscode/keybindings.json ~/.config/Code/User/
+ln -s /opt/config/.vscode/extensions ~/.vscode/
+
+# instalacion de java 8
+sudo apt install -y openjdk-8-jdk openjdk-8-jre
+
+echo -e "$GREEN [FINALIZO LA INSTALACION DE VERSIONES DE JAVA]"
+echo -e "PRESIONE UNA TECLA PARA CONTINUAR... $END_COLOR"
+read
+
+# instalacion de MAVEN
+
